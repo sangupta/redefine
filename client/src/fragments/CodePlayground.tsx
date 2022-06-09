650dd3fdc67d0ff8558f810a3769585942034893
Sandeep Gupta <sandy.pec@gmail.com>
2022-06-09 14:16:34 -0700

    UI niceness to live preview

--- client/src/fragments/CodePlayground.tsx
+++ client/src/fragments/CodePlayground.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
 interface Props {
@@ -21,10 +23,34 @@ export default class CodePlayground extends React.Component<Props> {
         }
 
         return <LiveProvider code={source} scope={{ ...ComponentLibrary }} >
-            <LivePreview Component={PreviewRenderer} />
+            <PreviewWrapper>
+                <LivePreview Component={PreviewRenderer} />
+            </PreviewWrapper>
             <LiveError />
-            <LiveEditor />
+            <CodeWrapper>
+                <LiveEditor />
+            </CodeWrapper>
         </LiveProvider>
     }
 
 }
+
+const PreviewWrapper = styled.div`
+    width: 100%;
+    max-width: 100%;
+    min-width: 100%;
+    padding: 10px;
+    border: 1px solid #e8e8e8;
+    overflow-x: scroll;
+    margin: 8px 0;
+`;
+
+const CodeWrapper = styled.div`
+    width: 100%;
+    max-width: 100%;
+    min-width: 100%;
+    padding: 10px;
+    border: 1px solid #e8e8e8;
+    background-color: #f5f5f5;
+    margin: 8px 0;
+`;
